febde99d975d14c83eb2babc9e60cd4761c08b16
Saritha H <jeslinjohnson26@gmail.com>
2022-06-09 15:25:36 +0530

    wdsd

---
+++ CO5/qn1.java
@@ -0,0 +1,24 @@
+package CO5;
+import java.applet.*;
+import java.awt.*;
+
+public class qn1 extends Applet{
+	public void paint (Graphics g)
+	{
+		g.setColor(Color.pink);
+		g.drawRect(120,50,100,100);
+		g.fillRect(120,50,100,100);
+		g.setColor(Color.blue);
+		g.drawOval(500, 500, 300, 150);
+		g.fillOval(120,50,100,100);
+		
+		
+		
+		
+	}
+	public static void main(String[] args){
+		
+	}
+
+	
+}
